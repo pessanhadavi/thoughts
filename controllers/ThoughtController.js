c8876e718b0a6d32744a5128bf305032d34be5b2
David Pessanha <davidppessanha@gmail.com>
2021-12-17 20:24:38 -0300

    Created thoughts form

--- controllers/ThoughtController.js
+++ controllers/ThoughtController.js
@@ -1,33 +1,37 @@
-const Thought = require("../models/Thought")
-const User = require("../models/User")
+const Thought = require("../models/Thought");
+const User = require("../models/User");
 
 module.exports = class ThoughtController {
   static async showThoughts(req, res) {
-    res.render("thoughts/home")
+    res.render("thoughts/home");
   }
 
   static async dashboard(req, res) {
-    const userId = req.session.userid
+    const userId = req.session.userid;
 
     const user = await User.findOne({
       where: {
-        id: userId,
+        id: userId
       },
       include: Thought,
-      plain: true,
-    })
+      plain: true
+    });
 
-    const thoughts = user.Thoughts.map((result) => result.dataValues)
+    const thoughts = user.Thoughts.map((result) => result.dataValues);
 
-    let emptyThoughts = true
+    let emptyThoughts = true;
 
     if (thoughts.length > 0) {
-      emptyThoughts = false
+      emptyThoughts = false;
     }
 
-    console.log(thoughts)
-    console.log(emptyThoughts)
+    console.log(thoughts);
+    console.log(emptyThoughts);
 
-    res.render('thoughts/dashboard', { thoughts, emptyThoughts })
+    res.render("thoughts/dashboard", { thoughts, emptyThoughts });
   }
-}
+
+  static createThought(req, res) {
+    res.render("thoughts/create");
+  }
+};
