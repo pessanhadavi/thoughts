2e71b958a40da5e78c1bb12feedf82fb4eedbc10
David Pessanha <davidppessanha@gmail.com>
2021-12-17 18:37:11 -0300

    Models are set

--- index.js
+++ index.js
@@ -8,16 +8,22 @@ const app = express();
 
 const conn = require("./db/conn");
 
+// Models
+const Thought = require("./models/Thought");
+const User = require("./models/User");
+
 // template engine
-app.engine("handlebars", exphbs())
-app.set("view engine", "handlebars")
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
 
 // receber resposta do body
-app.use (express.urlencoded( {
-  extended: true
-}))
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+);
 
-app.use(express.json())
+app.use(express.json());
 
 // session middleware
 app.use(
@@ -27,7 +33,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     store: new FileStore({
-      logFn: function() {},
+      logFn: function () {},
       path: require("path").join(require("os").tmpdir(), "sessions")
     }),
     cookie: {
@@ -37,24 +43,25 @@ app.use(
       httpOnly: true
     }
   })
-)
+);
 
 // flash messages
-app.use(flash())
+app.use(flash());
 
 // public path
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 // set session to res
 app.use((req, res, next) => {
   if (req.session.userid) {
-    res.locals.session = req.session
+    res.locals.session = req.session;
   }
 
-  next()
-})
+  next();
+});
 
 conn
+  // .sync({ force: true })
   .sync()
   .then(() => {
     app.listen(3000);
---
+++ models/User.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require("sequelize")
+
+const db = require("../db/conn");
+
+const User = db.define("User", {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    require: true
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    require: true
+  },
+  senha: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    require: true
+  }
+});
+
+module.exports = User;
