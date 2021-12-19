e519df7aef71762e4dee417de028aed7b862a88b
David Pessanha <davidppessanha@gmail.com>
2021-12-19 13:17:57 -0300

    Fixed mysql scheme name

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("thoughts2", "root", "", {
+const sequelize = new Sequelize("thoughts", "root", "", {
   host: "localhost",
   dialect: "mysql"
 });
